d99a747dcef4139564b786fcb68d4f1549e1ab98
AIR <46517472+git-air@users.noreply.github.com>
2023-04-02 16:52:10 +0900

    Add files via upload

---
+++ ios-architecture/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ios-architecture
+//
+//  Created by AIR on 2023/04/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
