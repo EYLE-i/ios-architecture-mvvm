c16b4f3df3e64852fc2d1ef6e1bb26d6fe9aa80d
AIR <46517472+git-air@users.noreply.github.com>
2023-04-17 01:18:08 +0900

    [add] tableViewを追加

--- ios-architecture/ViewController.swift
+++ ios-architecture/ViewController.swift
@@ -9,11 +9,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    let array = ["aaa", "iii", "uuu", "eee", "ooo"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        tableView.register(UINib(nibName: "PokemonTableViewCell", bundle: nil), forCellReuseIdentifier: "PokeCell")
+        tableView.rowHeight = 60
     }
-
-
 }
 
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return array.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PokeCell", for: indexPath) as! PokemonTableViewCell
+        cell.pokeLabel.text = array[indexPath.row]
+        return cell
+    }
+    
+    
+}
